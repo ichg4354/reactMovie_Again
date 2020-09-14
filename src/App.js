45b806b8241f5af0bb4b5787572ae5db396ef3f5
user <60804817+ichg4354@users.noreply.github.com>
2020-09-15 00:11:42 +0900

    try ro remember react states

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
-import "./App.css";
 
-function App() {
-  return <div>HELLO</div>;
+class App extends React.Component {
+  state = {
+    number: 0,
+  };
+  add = () => {
+    this.setState({ number: this.state.number + 1 });
+    console.log("+");
+  };
+  subtract = () => {
+    console.log("-");
+    this.setState({ number: this.state.number - 1 });
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.subtract}>minus</button>
+        <h1>HELLO the number is {this.state.number}</h1>;
+      </div>
+    );
+  }
 }
 
 export default App;
